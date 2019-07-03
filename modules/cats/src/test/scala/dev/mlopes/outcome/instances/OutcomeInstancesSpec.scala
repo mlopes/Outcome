a3164b1e2addd8a8b3a2ff340f8e49dd3cf79358
Marco Lopes <marco.paulo.lopes@gmail.com>
2019-07-03 16:59:44 +0100

    Add tests for Monad, Applicative and Functor functionality

--- modules/cats/src/test/scala/dev/mlopes/outcome/instances/OutcomeInstancesSpec.scala
+++ modules/cats/src/test/scala/dev/mlopes/outcome/instances/OutcomeInstancesSpec.scala
@@ -11,5 +11,26 @@ class OutcomeInstancesSpec extends WordSpec with Matchers with TypeCheckedTriple
     "provide pure" in {
       Monad[Outcome].pure("123") should ===(Failure("123"))
     }
+
+    "be flat mappable" in {
+      Monad[Outcome].flatMap(Failure("fail"))(_ => Success) shouldBe Success
+    }
+
+    "be mappable" in {
+      Monad[Outcome].map(Failure("fail"))(_ => 123) shouldBe Failure(123)
+    }
+
+    "be appliable" in {
+      Monad[Outcome].ap(Failure({s: String => s.length }))(Failure("hello world")) shouldBe Failure(11)
+    }
+
+    "be map2able" in {
+      Monad[Outcome].map2(Failure("Hello"), Failure("World"))((x: String, y: String) => s"$x $y") shouldBe Failure("Hello World")
+    }
+
+    "even lift" in {
+      val f = Monad[Outcome].lift((s: String) => s.length)
+      f(Failure("xyz")) shouldBe Failure(3)
+    }
   }
 }
